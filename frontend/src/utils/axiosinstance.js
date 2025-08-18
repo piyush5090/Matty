cf6053253e630b51ae7566f528be2112a79ec4e3
Piyush Govindani <govindanipiyush10@gmail.com>
2025-08-18 12:55:04 +0530

    Added backend url to frontend proxy

--- frontend/src/utils/axiosinstance.js
+++ frontend/src/utils/axiosinstance.js
@@ -4,7 +4,7 @@ const PORT = 5000;
 
 const axiosInstance = axios.create({
   // baseURL: `http://localhost:${PORT}`,
-  baseURL: `https://matty-design-tool-uh85.onrender.com/`,
+  baseURL: `https://matty.onrender.com/`,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
